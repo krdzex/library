eabfa237d5f963b7b7365d0bcbadc03a8e3d39ae
krdzex <58869690+krdzex@users.noreply.github.com>
2021-12-26 15:03:15 +0100

    Delete .history directory

--- .history/backend/config/config_20211222134416.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config()
-const config = {
-    port: process.env.PORT || 4400,
-    jwtSecret: process.env.JWT_SECRET ||  "YOUR_secret_key",
-    mongoUri: process.env.MONGO || MONGO=mongodb+srv://Paragon004:!400nogaraP$@cluster0.0smy7.mongodb.net/thirtyFive?retryWrites=true&w=majority
-}
-export default config;
